35066dc9ca6a251062f55d2af0bbf181cac26820
jinjie <lynnjinjie@gmail.com>
2020-09-05 23:51:07 +0800

    update: exercises 3.13-3.14

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
+const cors = require('cors') // 解决跨域问题
+const Person = require('./models/person')
+
 const app = express()
 
 app.use(express.json())
 app.use(express.static('build'))
 app.use(cors())
 
+// 打印控制台日志
 morgan.token('type', function (req, res) {
   return JSON.stringify(req.body)
 })
@@ -14,31 +18,36 @@ morgan.format('joke', ':method :url :status - :response-time ms :type')
 
 app.use(morgan('joke'))
 
-let persons = [
-  { name: 'Arto Hellas', number: '040-123456', id: 1 },
-  { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-  { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-  { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-]
+// let persons = [
+//   { name: 'Arto Hellas', number: '040-123456', id: 1 },
+//   { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+//   { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+//   { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+// ]
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person.find({}).then((persons) => {
+    res.json(persons)
+  })
 })
 
-app.get('/info', (req, res) => {
-  const info = `<div>Phonebook has info for ${persons.length} people</div>`
-  const date = `<div>${new Date()}</div>`
-  res.send(`${info}${date}`)
-})
+// app.get('/info', (req, res) => {
+//   const info = `<div>Phonebook has info for ${persons.length} people</div>`
+//   const date = `<div>${new Date()}</div>`
+//   res.send(`${info}${date}`)
+// })
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find((p) => p.id === id)
-  if (person) {
+  Person.findById(req.params.id).then((person) => {
     res.json(person)
-  } else {
-    res.status(404).end()
-  }
+  })
+  // const id = Number(req.params.id)
+  // const person = persons.find((p) => p.id === id)
+  // if (person) {
+  //   res.json(person)
+  // } else {
+  //   res.status(404).end()
+  // }
 })
 
 app.delete('/api/persons/:id', (req, res) => {
@@ -49,25 +58,26 @@ app.delete('/api/persons/:id', (req, res) => {
 
 app.post('/api/persons', (req, res) => {
   const body = req.body
-  const id = Math.random() * 1000
+  // const id = Math.random() * 1000
   if (!body.name || !body.number) {
     res.status(400).json({
       error: 'name or number missing',
     })
   }
-  const name = persons.find((p) => p.name === body.name)
-  if (name) {
-    res.status(400).json({
-      error: 'name must be unique',
-    })
-  }
-  const person = {
-    id: Math.floor(id),
+  // const name = persons.find((p) => p.name === body.name)
+  // if (name) {
+  //   res.status(400).json({
+  //     error: 'name must be unique',
+  //   })
+  // }
+  const person = new Person({
     name: body.name,
     number: body.number,
-  }
-  persons = persons.concat(person)
-  res.json(persons)
+  })
+
+  person.save().then((savePersons) => {
+    res.json(savePersons)
+  })
 })
 
 app.put('/api/persons/:id', (req, res) => {
---
+++ models/person.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URL
+
+console.log('connect to', url)
+
+mongoose
+  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB', error.message)
+  })
+
+const personShema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+// 去除mongoDB的_id 用 id代替
+personShema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
+module.exports = mongoose.model('Person', personShema)
